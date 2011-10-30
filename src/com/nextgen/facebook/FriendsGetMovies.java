3788b36ffa412929e47c74e818c7007843048735
Vaugan Nayagar <vaugann@gmail.com>
2011-10-30 18:12:42 +0200

    get movie details is working.
    Now what we need to do is only get the top 20 liked movies, and get details
    for those.
    Then display on screen.

--- src/com/nextgen/facebook/FriendsGetMovies.java
+++ src/com/nextgen/facebook/FriendsGetMovies.java
@@ -15,10 +15,16 @@ public class FriendsGetMovies {
     protected static JSONArray jsonFriendsArray;
     protected static JSONArray jsonMoviesArray;
     protected static JSONArray jsonMovieDetailsArray;
-    static  long friendsMoviesLikes[];
+    protected static JSONArray jsonMoviesDedupedArray;
+//    protected MovieDetails[] myMoviesList = new MovieDetails[20];
+    protected MovieDetails[] myMoviesList;
     static int movieCounter=0;
     static int totalFriends=0;
+    static int totalMovies=0;
+    static int detailsMoviesCtr=0;
     static int remainingFriends=0;
+    public static final int MAX_MOVIES  = 200;
+    
 
     /*
      * callback after friends are fetched via me/friends or fql query.
@@ -92,36 +98,31 @@ public class FriendsGetMovies {
 
                 //process this friend's movie list
                 if (jsonTempArray.length() > 0)
-                {
-                    
+                {                
                     jsonMoviesArray = concatArray(jsonMoviesArray,jsonTempArray);
-                    
-                    for (int i=0;i<jsonTempArray.length();i++)
-                    {
-                        movieId = jsonTempArray.getJSONObject(i).getLong("id");
-                        String movieName = jsonTempArray.getJSONObject(i).getString("name");
-//                        friendsMoviesLikes[movieCounter++] = movieId;
-//                        friendsMoviesLikes[movieCounter][1] = movieCounter++;
-                    }
                 }
 
                 //Request next friend's movie list
                 int position = totalFriends-remainingFriends;
-                if (position<50 && position >=0)
+                if (position<20 && position >=0)
                 {
                     long friendId = jsonFriendsArray.getJSONObject(totalFriends-remainingFriends).getLong("id");
                     Bundle params = new Bundle();
-                    params.putString("fields", "name, id");
+                    params.putString("fields", "name, id, category");
                     Utility.mAsyncRunner.request(friendId+"/movies", params, new MoviesRequestRequestListener());
                 }
                 else
                 {
                     Log.w(TAG, "All friends processed :-):::"+position);
                     
-                    /*Request the movie details*/
-                    Bundle params = new Bundle();
-                    params.putString("fields", "name, picture, link");
-                    Utility.mAsyncRunner.request(Long.toString(movieId), params, new MovieDetailsRequestListener());
+                    addMoviesToArray();
+                    
+                    
+//                    MoviesAddToArray();
+                    Log.w(TAG, "Movies sorted! ");
+                    
+//                    /*Request the movie details*/
+                    RequesMovieDetails();
                 }
             } catch (JSONException e) {
                 Log.w(TAG, "JSONException!!!  "+e);
@@ -152,24 +153,41 @@ public class FriendsGetMovies {
 
             try {
                 JSONObject jsonTempObject = new JSONObject(response);
-
-                //process this friend's movie list
+                long id = jsonTempObject.getLong("id");
+                String category = jsonTempObject.getString("category");
+                
+                //populate these details into myMoviesList
                 if (jsonTempObject.length() > 0)
                 {
-                    
-//                    jsonMovieDetailsArray = concatArray(jsonMovieDetailsArray,jsonTempObject);
-                    
-                        String pictureUrl = jsonTempObject.getString("picture");
-                        String movieName = jsonTempObject.getString("name");
-                        String movieLink = jsonTempObject.getString("link");
-                        
-                        Log.w(TAG, "movieName="+movieName);
-                        Log.w(TAG, "pictureUrl="+pictureUrl);
-                        Log.w(TAG, "movieLink="+movieLink);
+                    for(int i=0;i<myMoviesList.length;i++)
+                    {
+                        if ((myMoviesList[i].id == id) && (category.equals("Movie")))
+                        {
+                            myMoviesList[i].pictureUrl = jsonTempObject.getString("picture");
+                            myMoviesList[i].descr=jsonTempObject.getString("plot_outline");
+                            myMoviesList[i].category=category;
+                            Log.w(TAG, "myMoviesList["+i+"].pictureUrl"+myMoviesList[i].pictureUrl);
+                            Log.w(TAG, "myMoviesList["+i+"].descr"+myMoviesList[i].descr);
+                            Log.w(TAG, "myMoviesList["+i+"].category"+myMoviesList[i].category);
+                            break;
+                        }
+                    }
+                      
                 }
             } catch (JSONException e) {
                 Log.w(TAG, "JSONException!!!  "+e);
-                return;
+            }
+
+            //Request next movie details
+            if (detailsMoviesCtr++ < totalMovies)
+            {
+               Bundle params = new Bundle();
+               params.putString("fields", "name, picture, link, category, plot_outline");
+               Utility.mAsyncRunner.request(Long.toString(myMoviesList[detailsMoviesCtr].id), params, new MovieDetailsRequestListener());
+            }
+            else
+            {
+                Log.w(TAG, "All movies' details retrieved OK");
             }
 
         }
@@ -200,6 +218,88 @@ public class FriendsGetMovies {
         return result;
     }
     
+    private void addMoviesToArray()
+    {
+        boolean found=false;
+        long movieId=0;
+        String movieName;
+        int mCtr=0;
+        
+        //create and initialise array to worst case size, which is same as json array size.
+        totalMovies = jsonMoviesArray.length()>MAX_MOVIES?MAX_MOVIES:jsonMoviesArray.length();
+        
+        Log.w(TAG, "jsonMoviesArray.length()="+jsonMoviesArray.length());
+
+        myMoviesList = new MovieDetails[totalMovies];
+        for(int i = 0; i < myMoviesList.length; i++) {
+            myMoviesList[i] = new MovieDetails();
+         }
+        
+        /*From the full Json list of movies, which includes duplicates, we populate the myMoviesList. 
+         * Remove duplicates, but for each duplicate, increase the number of likes for that movie.*/
+        for (int i=0;i<jsonMoviesArray.length();i++)
+        {
+            found=false;
+
+            try {
+                movieId = jsonMoviesArray.getJSONObject(i).getLong("id");
+                movieName = jsonMoviesArray.getJSONObject(i).getString("name");
+//                Log.w(TAG, "jsonMoviesArray["+i+"] =  "+movieId+" "+ movieName);
+
+                for(int j=0;j<myMoviesList.length;j++)
+                {
+                    mCtr = i;
+                        
+                    if(myMoviesList[j].id == movieId)
+                    {
+                        //increment likes
+                        myMoviesList[j].likes++;
+                        found=true;
+                        Log.w(TAG, "mCtr="+mCtr+" Existing movie["+movieName+"], increasing likes to[ "+myMoviesList[j].likes);
+                        break;
+                    }
+                }
+                if (found == false)
+                {
+                     if (mCtr < totalMovies)
+                     {
+                         Log.w(TAG, "mCtr="+mCtr+" Adding new movie: "+movieName);
+                       myMoviesList[mCtr].id = movieId;
+                       myMoviesList[mCtr].name = movieName;
+                       myMoviesList[mCtr].likes = 1;
+                     }
+                     else
+                     {
+                         Log.w(TAG, "mCtr="+mCtr+" is larger than the max movie list size, so ignoring movie...");
+                     }
+                }
+
+            } catch (JSONException e) {
+                Log.w(TAG, "JSONException!!!  "+e);
+                return;
+            }
+        }
+
+        //print the movie list with the updated number of likes
+        for(int i=0;i<myMoviesList.length;i++)
+        {
+            Log.w(TAG, "myMoviesList["+i+"]="+" "+myMoviesList[i].id+" likes="+myMoviesList[i].likes+" "+myMoviesList[i].name);
+            Log.w(TAG, "myMoviesList["+i+"].pictureUrl="+myMoviesList[i].pictureUrl);
+        }
+    }
+    
+    private void RequesMovieDetails()
+    {
+//        for(int i=0;i<myMoviesList.length;i++)
+        {
+ //request first movie details
+            Bundle params = new Bundle();
+              params.putString("fields", "name, picture, link, category, plot_outline");
+              Utility.mAsyncRunner.request(Long.toString(myMoviesList[0].id), params, new MovieDetailsRequestListener());
+        }
+    }
+
+    
 //    private JSONArray concatArray(JSONArray... arrs)
 //    throws JSONException {
 //        JSONArray result = new JSONArray();
@@ -210,5 +310,19 @@ public class FriendsGetMovies {
 //        }
 //        return result;
 //    }
- 
+
+    
+    class MovieDetails{
+    long id;
+    int likes;
+    String name;
+    String pictureUrl;
+    String link;
+    String descr;
+    String category;
+    
+    
+    }
+    
+    
 }
